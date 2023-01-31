b8ada4e95f58fa022d15dc95e6aaf7e6a48a84ae
ianraphael <labicaniian@gmail.com>
2023-02-01 00:46:58 +0800

    Add add-todo component

--- frontend/src/app/components/add-todo/add-todo.component.ts
+++ frontend/src/app/components/add-todo/add-todo.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { v4 as uuidv4 } from 'uuid';
+import { ITodo } from 'src/app/services/todo';
 
 @Component({
-  selector: 'app-add-todo',
+  selector: 'add-todo',
   templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.scss']
+  styleUrls: ['./add-todo.component.scss'],
 })
 export class AddTodoComponent implements OnInit {
+  addToDoForm: FormGroup;
+  @Output() addToDo: EventEmitter<ITodo> = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.addToDoForm = this.formBuilder.group({
+      name: new FormControl<string | null>(null),
+      description: new FormControl<string | null>(null),
+      isHighPriority: new FormControl<boolean | null>(null),
+    });
   }
 
+  ngOnInit(): void {}
+
+  onAddToDo() {
+    this.addToDo.emit({ id: uuidv4(), ...this.addToDoForm.value });
+    this.addToDoForm.reset();
+  }
 }
