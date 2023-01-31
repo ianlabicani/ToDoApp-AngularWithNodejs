6d142a996a755b4a6275206996015ceb79aa4f69
ianraphael <labicaniian@gmail.com>
2023-01-31 17:40:05 +0800

    Connect and Add records to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,48 @@
 const express = require("express");
-
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const toDosSchema = {
+	id: Number,
+	name: String,
+	description: String,
+	isHighPriority: Boolean,
+};
+const toDo = mongoose.model("todo", toDosSchema);
+
+const defaultToDos = [
+	{
+		id: 1,
+		name: "Grocery Shopping",
+		description: "Pick up groceries for the week",
+		isHighPriority: false,
+	},
+	{
+		id: 2,
+		name: "Doctor's Appointment",
+		description: "Check-up with the dentist",
+		isHighPriority: true,
+	},
+	{
+		id: 3,
+		name: "Meeting with Team",
+		description: "Discuss project progress with team",
+		isHighPriority: false,
+	},
+];
+
+toDo.insertMany(defaultToDos, (err) => {
+	if (err) {
+		console.log("Insert Failed.");
+	} else {
+		console.log("Insert Successful.");
+	}
+});
+
+app.use(cors());
 app.use(express.json());
 
 app.listen(3000, (err) => {
