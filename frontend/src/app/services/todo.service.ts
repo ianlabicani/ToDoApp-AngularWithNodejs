d9c8376cd282a754fda551bba0dc2ac17f71498a
ianraphael <labicaniian@gmail.com>
2023-01-31 21:48:18 +0800

    Fix incorrect url for deleting a todo

--- frontend/src/app/services/todo.service.ts
+++ frontend/src/app/services/todo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { ITodo } from './todo';
 
 @Injectable({
@@ -16,7 +16,7 @@ export class TodoService {
   }
 
   deleteToDo(todo: ITodo): Observable<ITodo> {
-    const url = `${this.apiUrl}/${todo.id}`;
+    const url = `${this.apiUrl}/todo/${todo.id}`;
     return this.httpClient.delete<ITodo>(url);
   }
 }
