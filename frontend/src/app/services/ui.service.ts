c9fbcecb6ee67369cc01a71501a53b41499f3902
ianraphael <labicaniian@gmail.com>
2023-02-02 20:32:47 +0800

    Change default value

--- frontend/src/app/services/ui.service.ts
+++ frontend/src/app/services/ui.service.ts
@@ -5,7 +5,7 @@ import { Observable, Subject } from 'rxjs';
   providedIn: 'root',
 })
 export class UiService {
-  private showAddToDo: boolean = true;
+  private showAddToDo: boolean = false;
   private subject = new Subject<any>();
 
   constructor() {}
